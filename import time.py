ed65da1fbcd3f321065217d1bdb1dee57d1e59b8
Bru <61543927+berru-g@users.noreply.github.com>
2022-09-04 17:28:38 +0200

    Add files via upload

---
+++ import time.py
@@ -0,0 +1,16 @@
+import time
+from time import sleep
+from tkinter import CENTER
+import pyautogui
+
+#x, y = pyautogui.locateCenterOnScreen('mine.png')
+
+for n in range(5000):
+    pyautogui.click(49, 357)
+    pyautogui.click(218, 948)
+#    pyautogui.click(x, y)
+ #   sleep(0.1)
+print("Session executée avec succès\n1000 Click effectué")
+#STOP PUB****
+#if *****
+#pyautogui.click(394, 189)
